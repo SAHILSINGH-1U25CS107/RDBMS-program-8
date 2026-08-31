adfcc7d22f8b9b6841a12e1c6137a1c99a49ab70
SAHILSINGH-1U25CS107 <sahilto482000@gmail.com>
2026-08-31 11:14:46 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,39 +1,10 @@
--- Employee Aggregate Functions Assignment
+create database company3;
+use company3;
+create table employee (employeeID integer(5) primary key ,employeename varchar(10), department varchar(10),salary integer(6));
+desc employee;
+insert employee value("101","ravi","hr","25000"),("102","meena","it","40000"),("103","kumar","finance","35000"),("104","suresh","it","45000"),("105","latha","hr","30000");
+select count(*) from employee;
+select max(salary)from employee;
+select min(salary)from employee;
+select avg(salary)from employee;
 
--- Step 1: Create Employee table
-
-CREATE TABLE Employee (
-    EmployeeID INT PRIMARY KEY,
-    EmployeeName VARCHAR(50),
-    Department VARCHAR(30),
-    Salary INT
-);
-
--- Step 2: Insert the following records
-
-INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary)
-VALUES
-(101, 'Ravi', 'HR', 25000),
-(102, 'Meena', 'IT', 40000),
-(103, 'Kumar', 'Finance', 35000),
-(104, 'Suresh', 'IT', 45000),
-(105, 'Latha', 'HR', 30000);
-
-
--- Step 3: Write queries using COUNT(), MAX(), MIN()
--- and AVG() on the Salary field.
-
--- COUNT()
--- Write your query below:
-
-
--- MAX()
--- Write your query below:
-
-
--- MIN()
--- Write your query below:
-
-
--- AVG()
--- Write your query below:
