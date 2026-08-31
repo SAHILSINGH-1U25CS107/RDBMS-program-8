d3dd66f7fe9ff911774dae8ce493682896ac4f0a
SAHILSINGH-1U25CS107 <sahilto482000@gmail.com>
2026-08-31 11:22:50 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,5 +1,5 @@
-create database company4;
-use company4;
+create database company5;
+use company5;
 create table employee (employeeID integer(5) primary key ,employeename varchar(10), department varchar(10),salary integer(6));
 desc employee;
 insert employee value("101","ravi","hr","25000"),("102","meena","it","40000"),("103","kumar","finance","35000"),("104","suresh","it","45000"),("105","latha","hr","30000");
